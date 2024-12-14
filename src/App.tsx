c2e91efa2fc0df5473f3ccbf726adb03bed7a248
santiago Doval <santiagodovalb@gmail.com>
2024-12-14 16:29:15 -0300

    chore: fixed build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Router components
 import AddPill from './pages/AddPill/AddPill';
 import DeletePill from './pages/DeletePill/DeletePill';
 
