a1b1094e5f363b52393a0bdc10cfb3fba7e9f5ec
santiago Doval <santiagodovalb@gmail.com>
2024-12-14 16:16:12 -0300

    feat:added pasword

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Router components
 import AddPill from './pages/AddPill/AddPill';
 import DeletePill from './pages/DeletePill/DeletePill';
 
 const App: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null); // State indicating authentication
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      const password = prompt("Ingresar contraseña:"); // Prompt for password
+      if (password === import.meta.env.VITE_APP_PASSWORD) {
+        setIsAuthenticated(true);
+      } else {
+        alert("Incorrect password!");
+        setIsAuthenticated(false);
+      }
+    }
+  }, [isAuthenticated]); // Only run once on first render
+
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <h2>Accesso denegado</h2>
+        <p>Ingresá la contraseña nuevamente</p>
+      </div>
+    );
+  }
+
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* Route for adding a pill */}
           <Route path="/" element={<AddPill />} />
-
-          {/* Route for deleting a pill */}
           <Route path="/delete" element={<DeletePill />} />
         </Routes>
       </div>
     </Router>
   );
-}
+};
 
 export default App;
